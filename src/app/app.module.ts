02edcc44c8231d41f7a90958bc6836843ec9e4eb
kiranlasure <kiranlasure95@gmail.com>
2019-10-20 00:41:13 +0530

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { Login2Component } from './login2/login2.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent,],
+  imports:      [ BrowserModule, FormsModule, FlexLayoutModule],
+  declarations: [ AppComponent, LoginComponent, Login2Component,],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
