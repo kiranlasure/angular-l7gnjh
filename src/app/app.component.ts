0fb0dfddf4fe3675f423459800843f7b5213ac37
kiranlasure <kiranlasure95@gmail.com>
2019-10-20 23:21:51 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  providers: [EmployeeService]
 })
+
 export class AppComponent  {
   name = 'Angular';
 
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -11,7 +11,7 @@ export class EmployeeService {
       {"id": 3, "Name" : "Nikhil", "Gender":"Male"},
       {"id": 4, "Name" : "Nitin", "Gender":"Female"},
       {"id": 5, "Name" : "Rakhi", "Gender":"Female"},
-      {"id": 6, "Name" : "Ritesh", "Gender":"Female"}
+      {"id": 6, "Name" : "Ritesh", "Gender":"Female"},
       {"id": 7, "Name" : "Dipika", "Gender":"Female"}
     ]
   }
