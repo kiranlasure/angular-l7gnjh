c4fe93d14aa0a8e8647b2d85e62159208a737bda
kiranlasure <kiranlasure95@gmail.com>
2019-10-20 23:08:53 +0530

    1 file modified

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -4,11 +4,15 @@ import { Injectable } from '@angular/core';
 export class EmployeeService {
 
   getEmployees() {
+    
     return [
       {"id": 1, "Name" : "Rushikesh", "Gender":"Male"},
       {"id": 2, "Name" : "Radhika", "Gender":"Female"},
       {"id": 3, "Name" : "Nikhil", "Gender":"Male"},
       {"id": 4, "Name" : "Nitin", "Gender":"Female"},
+      {"id": 5, "Name" : "Rakhi", "Gender":"Female"},
+      {"id": 6, "Name" : "Ritesh", "Gender":"Female"}
+      {"id": 7, "Name" : "Dipika", "Gender":"Female"}
     ]
   }
   constructor() { }
