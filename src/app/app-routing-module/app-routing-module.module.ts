edab4966ba1a6520a720ee20efd6c67332c64e8a
kiranlasure <kiranlasure95@gmail.com>
2019-10-20 04:35:15 -0700

    2 files modified, 1 deleted

--- src/app/app-routing-module/app-routing-module.module.ts
+++ src/app/app-routing-module/app-routing-module.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent} from './app.component';
-import { LoginComponent } from './login/Login.component';
-import { Login2Component } from './login2/Login2.component';
+import { LoginComponent } from '../login/Login.component';
+import { Login2Component } from '../login2/Login2.component';
+
 const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'login2', component: Login2Component}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from "@angular/flex-layout";
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { Login2Component } from './login2/login2.component';
 
-import { AppRoutingModule, routingComponent} from './app-routing.module';
+import { AppComponent }            from './app.component';
 
-const appRoutes: Routes = [
-  {  },
-  
-];
+import { AppRoutingModule, routingComponent} from '.app/app-routing.module';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, FlexLayoutModule, AppRoutingModule],
-  declarations: [ AppComponent, routingComponent],
-  bootstrap:    [ AppComponent ]
+
+  declarations: [ AppComponent,
+   routingComponent],
+
+  bootstrap:    [ AppComponent ],
+
+  providers: [],
 })
 export class AppModule { }
