ae1f3ed2eb5e7ac14e17290daaae16396ef685ef
kiranlasure <kiranlasure95@gmail.com>
2019-10-20 03:58:43 -0700

    2 files modified

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent} from './app.component';
+import { LoginComponent } from './login/Login.component';
+import { Login2Component } from './login2/Login2.component';
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'login2', component: Login2Component}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+
+  exports: [RouterModule],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    Login2Component
+  ]
 })
 export class AppRoutingModule { }
+export const routingComponent = [LoginComponent,Login2Component]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { Login2Component } from './login2/login2.component';
 
+import { AppRoutingModule, routingComponent} from './app-routing.module';
+
+const appRoutes: Routes = [
+  {  },
+  
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, FlexLayoutModule],
-  declarations: [ AppComponent, LoginComponent, Login2Component,],
+  imports:      [ BrowserModule, FormsModule, FlexLayoutModule, AppRoutingModule],
+  declarations: [ AppComponent, routingComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
